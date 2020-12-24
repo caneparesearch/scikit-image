2560704242d1dbaff44d999e6bd727ff5297cec7
Tara <tara.13.1996@gmail.com>
2020-12-24 10:57:47 +0800

    Adding blob feature

--- skimage/feature/__init__.py
+++ skimage/feature/__init__.py
@@ -24,7 +24,7 @@ from .censure import CENSURE
 from .orb import ORB
 from .match import match_descriptors
 from .util import plot_matches
-from .blob import blob_dog, blob_log, blob_doh, blob_glog
+from .blob import blob_dog, blob_log, blob_doh, blob_alog
 from .haar import (haar_like_feature, haar_like_feature_coord,
                    draw_haar_like_feature)
 from ._basic_features import multiscale_basic_features
@@ -88,7 +88,7 @@ __all__ = ['canny',
            'blob_dog',
            'blob_doh',
            'blob_log',
-           'blob_glog'
+           'blob_alog'
            'haar_like_feature',
            'haar_like_feature_coord',
            'draw_haar_like_feature',
--- skimage/feature/blob.py
+++ skimage/feature/blob.py
@@ -534,11 +534,11 @@ def blob_log(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
 
     return _prune_blobs(lm, overlap, sigma_dim=sigma_dim)
 
-def blob_glog(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
+def blob_alog(image, min_sigma=1, max_sigma=50, num_sigma=10, threshold=.2,
              overlap=.5, log_scale=False, *, exclude_border=False):
     r"""Finds blobs in the given grayscale image.
 
-    Blobs are found using the Generalized Laplacian of Gaussian (gLoG) method.
+    Blobs are found using the anisotropic Laplacian of Gaussian (aLoG) method.
     For each blob found, the method returns its coordinates and the standard
     deviation of the Gaussian kernel that detected the blob. This 
     means we find iterative anisotropic gaussian recussively.
